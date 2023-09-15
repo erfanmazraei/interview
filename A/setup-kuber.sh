8d46e51495406a84fd73843c3ba1f07679f864bd
mazraei <erfanmazraei@gmail.com>
2023-09-15 15:53:43 +0330

    fix

--- A/setup-kuber.sh
+++ A/setup-kuber.sh
@@ -5,6 +5,7 @@ set -x
 main () {
     pre_setup
     setup_kubernetes
+    move_config_file
 }
 
 pre_setup () {
@@ -12,17 +13,27 @@ pre_setup () {
     sudo apt-get update
     sudo apt-get install git -y
     sudo apt-get install python3-venv -y
+    sudo apt-get install python3 -y
+    pip install virtualenv
+    python3 -m virtualenv envsp
+
 }
 
 setup_kubernetes () {
     git clone https://github.com/kubernetes-sigs/kubespray.git
     cd kubespray
-    source venv/bin/activate
+    source venv/bin/activate || source envsp/bin/activate
     pip install -r requirements.txt
     declare -r CLUSTER_FOLDER='my-cluster'
     cp -rfp inventory/local inventory/$CLUSTER_FOLDER
     USERNAME=$(whoami)
     ansible-playbook -i inventory/$CLUSTER_FOLDER/hosts.ini --connection=local -b -v cluster.yml
+}
+
+move_config_file () {
+    mkdir -p $HOME/.kube
+    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    sudo chown $(id -u):$(id -g) $HOME/.kube/config
 }
 
 main
